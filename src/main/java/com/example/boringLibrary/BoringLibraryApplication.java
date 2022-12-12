bc3ee7f7f9ad403977117f1f2c49ff078a92b177
Alessio Wright <alessiowright@outlook.com>
2022-12-12 10:56:12 +0000

    added spring boot template

---
+++ src/main/java/com/example/boringLibrary/BoringLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.boringLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoringLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoringLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boringLibrary/BoringLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boringLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoringLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
